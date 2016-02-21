a0cdb33718e7253c1ab3e77e8a631a44ff48dcc5
Ben Houshmand <nayab9@gmail.com>
2016-02-21 23:20:41 +0000

    Implemented the list maker to generate a set of matched listings to products.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from util.io import IOManager
 from util.jsonParser import JSONParser
 from brains.modelMaker import ModelMaker
+from brains.listingMaker import ListingMaker
 
 #intialize IO manager with a filename (hardcoded for now)
 #TODO: pass filename as a paramter with a flag
@@ -17,7 +18,7 @@ fileInput2.printFilename()
 #send fileinput to a json parser which will handle processing data
 parsedData1 = JSONParser(fileInput1.fileContent)
 #parsedData1.printData()
-
+parsedData2 = JSONParser(fileInput2.fileContent)
 #now have a data structure containing all data of products
 
 #Go through each product, and make a dict with key being the MODEL NUMBER!
@@ -26,6 +27,7 @@ modelDict = ModelMaker(parsedData1.parsedData)
 # now I have a nice hash table of my products (note, this could be huge, putting it on disk in buckets is a ram efficient solution
 
 #go through each listing.txt item, send the listing to a parser
+listingList = ListingMaker(parsedData2.parsedData, modelDict.modelDict)
 
 #the parser will take the listing, pull its 'title'
 #the title for the list is split into its keywords based on some critera
